da4543615ce28898826ae69988c7761ba2e0038b
mtts021 <matteusganha@gmail.com>
2023-04-24 12:42:14 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import adminRouter from './routes/admin-router';
 import sessionsRouter from './routes/sessions-router';
 import jwtAuthenticationMiddleware from './app/middlewares/jwt-authentication.middleware';
 import errorHandler from './app/middlewares/error-handler.middleware';
 
 const app = express();
-
+app.use(cors({
+    origin: 'http://localhost:3000/'
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.get('/status', (req, res)=> {
@@ -18,5 +21,5 @@ app.use(errorHandler);
 
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000');
+    console.log('http://localhost:3001');
 });
