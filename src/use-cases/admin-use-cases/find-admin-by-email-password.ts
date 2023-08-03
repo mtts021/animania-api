85eacdf00a9618f26786e2eb50bd42bf98f9ecea
CCerrer <mathferrer17ea@gmail.com>
2023-08-03 16:54:26 -0300

    Adicionado chamada para verificacoes de email e senha

--- src/use-cases/admin-use-cases/find-admin-by-email-password.ts
+++ src/use-cases/admin-use-cases/find-admin-by-email-password.ts
@@ -2,11 +2,15 @@ import Admin from 'app/entities/admin';
 import {BadRequestError} from '../../app/helpers/api-error';
 import AdminRepository from 'app/repositories/admin-repository';
 import bcrypt from 'bcrypt';
+import { Validator } from '../../app/helpers/input-validation';
 
 export default class FindAdminByEmailAndPassword {
     constructor(private adminRepository: AdminRepository){}
 
     async execute(email: string, password: string): Promise< Admin> {
+        await Validator.emailValidation(email);
+        await Validator.passwordValidation(password);
+        
         const admin = await this.adminRepository.findByEmail(email);
         if(!admin){
             throw new BadRequestError('Incorrect email');
